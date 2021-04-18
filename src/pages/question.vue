303505c7d2d3a295ade53ff0ac99dfdb7823d044
T480s <techcom.ec20@gmail.com>
2021-04-19 04:08:16 +0700

    add: Card Component

--- src/pages/question.vue
+++ src/pages/question.vue
@@ -1,30 +1,7 @@
 <template>
   <div class="q-pa-md">
     <!-- card -->
-    <q-card class="q-ma-md" v-for="(item, index) in question.Questions" :key="index">
-      <!-- title question -->
-      <q-card-section class="q-ml-md">
-        <span>
-          <div>
-            <b class="q-mr-xs">ข้อที่: {{ index + 1 }}</b
-            >{{ item.Question }}
-          </div>
-        </span>
-      </q-card-section>
-      <!-- content -->
-      <q-card-section class="q-pt-none q-ml-md">
-        <div class="row">
-          <div v-if="item.Type === null">
-            <!-- radio -->
-            <q-option-group class="q-ml-sm" :options="item.Answers" type="radio" v-model="item.Result"/>
-          </div>
-          <!-- input -->
-          <div class="full-width q-mb-sm" v-else>
-             <q-input class="q-ml-lg" outlined v-model="item.Result" dense/>
-          </div>
-        </div>
-      </q-card-section>
-    </q-card>
+    <CardComponent v-for="(item, index) in question.Questions" :key="item" :question="item" :index="index"></CardComponent>
     <!-- end card -->
   </div>
 </template>
@@ -32,15 +9,19 @@
 <script>
 import { ref, reactive, watch } from 'vue';
 import { state } from '../hook/Question';
+import CardComponent from '../components/Question/Card';
 
 export default {
+  components: {
+    CardComponent,
+  },
   setup() {
     const color = ref('cyan');
     const { question } = state();
 
-    watch(question.Questions.Result, (val) => {
-      console.log(val);
-    });
+    // watch(question.Questions.Result, (val) => {
+    //   console.log(val);
+    // });
 
     const options = reactive([
       { label: 'Battery too low', value: 'bat' },
