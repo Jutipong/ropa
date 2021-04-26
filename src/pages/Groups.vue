e6c576615702c6c7275571f859488874896c778b
T480s <techcom.ec20@gmail.com>
2021-04-27 01:23:39 +0700

    update: code

--- src/pages/Groups.vue
+++ src/pages/Groups.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="q-pa-md">
-    <div v-if="isLoading" class="row q-gutter-sm">
+    <div v-if="loadingPage" class="row q-gutter-sm">
       <!-- Loading -->
       <q-spinner-puff color="primary" size="lg" />
       <div class="q-pt-sm text-primary">กำลังโหลดข้อมูล</div>
     </div>
     <div v-else>
       <!-- content -->
-      <q-table :rows="store" :columns="columns" row-key="id" :loading="isLoading" :pagination="initialPagination">
+      <q-table :rows="store" :columns="columns" row-key="id" :loading="loading" v-model:pagination="pagination">
         <!-- slot top -->
         <template v-slot:top="props">
           <q-btn flat round :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'" @click="props.toggleFullscreen" class="q-ml-md" />
@@ -20,7 +20,7 @@
               <q-icon name="search" />
             </template>
           </q-input>
-          <q-btn round class="q-ml-md" color="secondary" icon="eva-refresh-outline" :loading="isLoading" :disable="isLoading" />
+          <q-btn round class="q-ml-md" color="secondary" icon="eva-refresh-outline" :loading="loading" :disable="loading" />
         </template>
 
         <!-- <template v-slot:pagination="scope">
@@ -57,14 +57,15 @@
 </template>
 
 <script>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { date } from 'quasar';
 import stateUse from '../hook/Groups/state';
 import actionUse from '../hook/Groups/action';
 
 export default {
   setup() {
-    const { isLoading, store } = stateUse;
+    const loadingPage = ref(true);
+    const { loading, store } = stateUse;
     const { GetGroups } = actionUse;
     const columns = [
       { name: 'Name', label: 'Name', align: 'center', field: 'Name', sortable: true },
@@ -88,16 +89,93 @@ export default {
       { name: 'UpdateBy', label: 'UpdateBy', align: 'center', field: 'UpdateBy', sortable: true },
     ];
 
-    const initialPagination = {
+    const rows = ref([]);
+    const filter = ref('');
+    const pagination = ref({
       sortBy: 'desc',
       descending: false,
       page: 1,
-      rowsPerPage: 10,
-      // rowsNumber: xx if getting data from a server
-    };
+      rowsPerPage: 3,
+      rowsNumber: 10,
+    });
 
-    onMounted(() => GetGroups());
-    return { isLoading, store, columns, initialPagination };
+    function fetchFromServer(startRow, count, filter, sortBy, descending) {
+      const data = filter ? store.filter((row) => row.name.includes(filter)) : store.slice();
+
+      // handle sortBy
+      if (sortBy) {
+        const sortFn =
+          sortBy === 'desc'
+            ? descending
+              ? (a, b) => (a.name > b.name ? -1 : a.name < b.name ? 1 : 0)
+              : (a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0)
+            : descending
+            ? (a, b) => parseFloat(b[sortBy]) - parseFloat(a[sortBy])
+            : (a, b) => parseFloat(a[sortBy]) - parseFloat(b[sortBy]);
+        data.sort(sortFn);
+      }
+
+      return data.slice(startRow, startRow + count);
+    }
+
+    function onRequest(props) {
+      const { page, rowsPerPage, sortBy, descending } = props.pagination;
+      const filter = props.filter;
+
+      loading.value = true;
+
+      // emulate server
+      setTimeout(() => {
+        // update rowsCount with appropriate value
+        pagination.value.rowsNumber = getRowsNumberCount(filter);
+
+        // get all rows if "All" (0) is selected
+        const fetchCount = rowsPerPage === 0 ? pagination.value.rowsNumber : rowsPerPage;
+
+        // calculate starting row of data
+        const startRow = (page - 1) * rowsPerPage;
+
+        // fetch data from "server"
+        const returnedData = fetchFromServer(startRow, fetchCount, filter, sortBy, descending);
+
+        // clear out existing data and add new
+        rows.value.splice(0, rows.value.length, ...returnedData);
+
+        // don't forget to update local pagination object
+        pagination.value.page = page;
+        pagination.value.rowsPerPage = rowsPerPage;
+        pagination.value.sortBy = sortBy;
+        pagination.value.descending = descending;
+
+        // ...and turn of loading indicator
+        loading.value = false;
+        loadingPage.value = false;
+      }, 1500);
+    }
+
+    function getRowsNumberCount(filter) {
+      if (!filter) {
+        return store.length;
+      }
+      let count = 0;
+      store.forEach((treat) => {
+        if (treat.name.includes(filter)) {
+          ++count;
+        }
+      });
+      return count;
+    }
+
+    onMounted(() => {
+      // get initial data from server (1st page)
+      onRequest({
+        pagination: pagination.value,
+        filter: undefined,
+      });
+    });
+
+    // onMounted(() => GetGroups());
+    return { loadingPage, loading, store, columns, pagination };
   },
 };
 </script>
