387f7bc027fa93dc7abe6e5dee40cf2eb1a7423d
T480s <techcom.ec20@gmail.com>
2021-05-02 17:10:12 +0700

    refactor code

--- src/pages/Groups.vue
+++ src/pages/Groups.vue
@@ -1,62 +1,57 @@
 <template>
   <div class="q-pa-md">
-    <div v-if="loadingPage" class="row q-gutter-sm">
-      <!-- Loading -->
-      <q-spinner-puff color="primary" size="lg" />
-      <div class="q-pt-sm text-primary">กำลังโหลดข้อมูล</div>
+    <!-- content -->
+    <div class="row q-mb-md">
+      <!-- <q-btn color="primary" icon="check" label="OK" @click="onAdd" /> -->
     </div>
-    <div v-else>
-      <!-- content -->
-      <div class="row q-mb-md">
-        <!-- <q-btn color="primary" icon="check" label="OK" @click="onAdd" /> -->
-      </div>
-      <q-table
-        row-key="id"
-        :rows="rows"
-        :columns="columns"
-        :loading="loading"
-        @request="onRequest"
-        v-model:pagination="pagination"
-      >
-        <!-- slot top -->
-        <template v-slot:top="props">
-          <q-btn
-            flat
-            round
-            :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
-            @click="props.toggleFullscreen"
-            class="q-ml-md"
-          />
-          <!-- <q-space /> -->
-          <q-separator vertical spaced />
-          <div class="col-4 q-table-title q-pl-sm xs-hide sm-hide">Gropus</div>
-          <q-space />
-          <q-input
-            clearable
-            dense
-            bordered
-            placeholder="ค้นหา"
-            v-model="filter"
-            @keyup.enter="onRequest({ pagination })"
-          >
-            <template v-slot:append>
-              <q-icon name="search" />
-            </template>
-          </q-input>
-          <q-btn
-            round
-            class="q-ml-md"
-            color="secondary"
-            icon="eva-refresh-outline"
-            :loading="loading"
-            :disable="loading"
-            @click="onRequest({ pagination })"
-          />
-        </template>
-        <!-- <template v-slot:pagination="scope"> Page: {{ scope.pagination.page }}/ {{ scope.pagesNumber }} </template> -->
-      </q-table>
-      <!-- end content -->
-    </div>
+    <q-table
+      row-key="id"
+      :rows="rows"
+      :columns="columns"
+      :loading="loading"
+      @request="onRequest"
+      v-model:pagination="pagination"
+      color="primary"
+    >
+      <!-- slot top -->
+      <template v-slot:top="props">
+        <q-btn
+          flat
+          round
+          :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+          @click="props.toggleFullscreen"
+          class="q-ml-md"
+        />
+        <!-- <q-space /> -->
+        <q-separator vertical spaced />
+        <div class="col-4 q-table-title q-pl-sm xs-hide sm-hide">Gropus</div>
+        <q-space />
+        <q-input
+          clearable
+          dense
+          bordered
+          placeholder="ค้นหา"
+          :disable="loading"
+          v-model="filter"
+          @keyup.enter="onRequest({ pagination })"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+        <q-btn
+          round
+          class="q-ml-md"
+          color="secondary"
+          icon="eva-refresh-outline"
+          :loading="loading"
+          :disable="loading"
+          @click="onRequest({ pagination })"
+        />
+      </template>
+      <!-- <template v-slot:pagination="scope"> Page: {{ scope.pagination.page }}/ {{ scope.pagesNumber }} </template> -->
+    </q-table>
+    <!-- end content -->
   </div>
 </template>
 
@@ -69,32 +64,12 @@ import actionUse from '../hook/Groups/action';
 
 export default {
   setup() {
+    //value utility
     const $q = useQuasar();
-    const loadingPage = ref(true);
-    const { loading, store } = stateUse;
+    const { loading, columns } = stateUse;
     const { GetGroups } = actionUse;
-    const columns = [
-      { name: 'Name', label: 'Name', align: 'center', field: 'Name', sortable: true },
-      {
-        name: 'CreateDate',
-        label: 'CreateDate',
-        align: 'center',
-        field: 'CreateDate',
-        sortable: true,
-        format: (val) => (val ? `${date.formatDate(val, 'DD/MM/YYYY HH:mm:ss')}` : '-'),
-      },
-      { name: 'CreateBy', label: 'CreateBy', align: 'center', field: 'CreateBy', sortable: true },
-      {
-        name: 'UpdateDate',
-        label: 'UpdateDate',
-        align: 'center',
-        field: 'UpdateDate',
-        sortable: true,
-        format: (val) => (val ? `${date.formatDate(val, 'DD/MM/YYYY HH:mm:ss')}` : '-'),
-      },
-      { name: 'UpdateBy', label: 'UpdateBy', align: 'center', field: 'UpdateBy', sortable: true },
-    ];
 
+    //value in page
     const rows = ref([]);
     const filter = ref('');
     const pagination = ref({
@@ -158,13 +133,9 @@ export default {
       await onRequest({
         pagination: pagination.value,
       });
-
-      setTimeout(() => {
-        loadingPage.value = false;
-      }, 1000);
     });
 
-    return { loadingPage, loading, store, rows, columns, pagination, onRequest, filter };
+    return { loading, rows, columns, pagination, filter, onRequest };
   },
 };
 </script>
