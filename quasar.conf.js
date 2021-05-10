8c0261c6117db2edee80d7d031fa1d0e565b7939
@Macbook <techcom.ec20@gmail.com>
2021-05-10 14:12:07 +0700

    Clean Code

--- quasar.conf.js
+++ quasar.conf.js
@@ -73,7 +73,7 @@ https: module.exports = configure(function (ctx) {
     devServer: {
       https: false,
       port: 8080,
-      open: true, // opens browser window automatically
+      open: false, // opens browser window automatically
     },
 
     // https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-framework
