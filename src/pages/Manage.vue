bcb80fc86d1fa420c66372c1586061e05e7d9d22
@Macbook <techcom.ec20@gmail.com>
2021-05-11 18:15:01 +0700

    Update: Page Manage

--- src/pages/Manage.vue
+++ src/pages/Manage.vue
@@ -1,15 +1,51 @@
 <template>
-    <div>
+  <div class="q-pa-md">
+    <!-- content -->
 
+      <q-card>
+        <q-tabs
+          v-model="tab"
+          align="left"
+          inline-label
+          outside-arrows
+          mobile-arrows
+          active-color="primary"
+          indicator-color="primary"
+        >
+          <q-tab name="mails" label="Mails" />
+          <q-tab name="alarms" label="Alarms" />
+          <q-tab name="movies" label="Movies" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="mails">
+            <div class="text-h6">Mails</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="alarms">
+            <div class="text-h6">Alarms</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+
+          <q-tab-panel name="movies">
+            <div class="text-h6">Movies</div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-card>
     </div>
 </template>
+<!-- end content -->
 
 <script>
+import { ref } from 'vue';
 export default {
-    setup () {
-        
-
-        return {}
-    }
-}
+  setup() {
+    const tab = ref('mails');
+    return { tab };
+  },
+};
 </script>
